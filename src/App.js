d258976812a22a4d41abb9b110b13cb0f744f0b0
ashrqat <ashrqat.fawzy@gmail.com>
2025-10-19 18:46:32 +0300

    Use HashRouter for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
 import "./App.css";
 import "./global.scss";
 import { useState, useEffect } from "react";
--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-  import { Form } from 'react-bootstrap';
-  import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter } from 'react-router-dom'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+      <App />
+
   </React.StrictMode>
 );
-
-
